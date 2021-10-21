02efc4dab3dc151aa27b8d01460a4e642aec2591
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-10-21 23:50:56 +0200

    improved modified index.js

--- index.js
+++ index.js
@@ -4,49 +4,49 @@ const express = require('express');
 const app = express(),
 morgan = require('morgan');
 
+// Use the Morgan middleware library to log all requests (instead of using 
+//the fs module to write to a text file).
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to mycomedyFlix app!');
+});
+
+app.get('/secreturl', (req, res) => {
+    res.send('This is a secret url with super top-secret content.');
+});
+
 //Created a topMovies list.
-let topBooks = [
+let topMovies = [
     {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
+    title: 'The Lord of the Rings: The Return of the King',
+    year: '2003',
     },
     {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
+    title: 'Pulp Fiction',
+    year: '1994',
     },
     {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    year: '2001',
     }
 ];
 
 // GET requests
 app.get('/', (req, res) => {
-    res.send('Welcome to my book club!');
+    res.send('Welcome to mycomedyFlix app!');
 });
 
 app.get('/documentation', (req, res) => {                  
     res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-app.get('/books', (req, res) => {
-    res.json(topBooks);
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
 });
 
 // Serving static files
 app.use(express.static('public'));
-
-// Use the Morgan middleware library to log all requests (instead of using 
-//the fs module to write to a text file).
-app.use(morgan('common'));
-
-app.get('/', (req, res) => {
-    res.send('Welcome to my app!');
-});
-
-app.get('/secreturl', (req, res) => {
-    res.send('This is a secret url with super top-secret content.');
-});
 
 // error-handling middleware function that will log all application-level errors to the terminal
 app.use((err, req, res, next) => {
