79573df24cd8b2315d7143f3d9bfb93eeceba407
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-10-24 09:36:06 +0200

    Improved API Endpoints

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
+
 //Require
 const express = require('express');
-const app = express();
+
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+app = express();
 app.use(express.static('public'));
-app.use(morgan('common'));
-
+app.use(bodyParser.json());
 
 // Use the Morgan middleware library to log all requests (instead of using 
 //the fs module to write to a text file).
 app.use(morgan('common'));
-
 app.get('/', (req, res) => {
     res.send('Welcome to mycomedyFlix app!');
 });
@@ -25,62 +25,242 @@ let topTenMovies = [
     {
         title: 'Pitch Perfect',
         year: '2012',
-        genre: ['Comedy', 'Music', 'Romance'],
-        director: 'Jason Moore '
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Music'],
+                description:'   '
+            },
+            {
+                id:3,
+                name:'Romance',
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Jason Moore '],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'Solo: A Star Wars Story',
         year: '2018',
-        genre: ['Comedy','Action', 'Adventure'],
-        director: 'Ron Howard'
+        genre:[
+        {
+            id:1,
+            name:['Comedy'],
+            description:'   '
+        },
+        {
+            id:2,
+            name:['Action'],
+            description:'   '
+        },
+        {
+            id:3,
+            name:'Adventure',
+            description:'   '
+        },
+        ],
+        director:[
+            {
+                id:1,
+                name:['Ron Howard'],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'Detective MJ: Shadow of a Hero',
         year: '2020',
-        genre: ['Comedy', 'Action'],
-        director: 'Morris D. Small'
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Action'],
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Morris D. Small'],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'Mum, Dad, Meet Sam',
         year: '2014',
-        genre: ['Comedy', 'Romance'],
-        director: 'Tony Sebastian Ukpo'
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Romance'],
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Tony Sebastian Ukpo'],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'The Nutty Professor',
         year: '1963',
-        genre: ['Comedy','Romance'],
-        director: 'Jerry Lewis'
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Romance'],
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Jerry Lewis'],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'Game Over, Man!',
         year: '2018',
-        genre: ['Comedy', 'Action'],
-        director: 'Kyle Newacheck'
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Action'],
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Kyle Newacheck'],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'No Holds Bars Comedy Presents: Pilot',
         year: '2012',
-        genre: ['Comedy', 'Action'],
-        director: 'Derrick Comedy'
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Action'],
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Derrick Comedy'],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'The Day After Quarantine',
         year: '2021',
-        genre: ['Comedy', 'Adventure'],
-        director: 'Gary Trousdale'
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Adventure'],
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Gary Trousdale'],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'No Filter the Film',
         year: '2015',
-        genre: ['Comedy'],
-        director: 'Barry Williams'
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Adventure'],
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Barry Williams'],
+                biography:'   '
+            },
+            ],
     },
     {
         title: 'All Star Comedy Jam',
         year: '2009',
-        genre: ['Comedy', 'Documentary'],
-        director: 'Leslie Small'
+        genre:[
+            {
+                id:1,
+                name:['Comedy'],
+                description:'   '
+            },
+            {
+                id:2,
+                name:['Documentary'],
+                description:'   '
+            },
+            ],
+        director:[
+            {
+                id:1,
+                name:['Leslie Small'],
+                biography:'   '
+            },
+            ],
     }
 ];
 
@@ -108,15 +288,31 @@ app.get('/movies/:year', (req, res) => {
     res.send('Successful GET request returning movie by year');
 });
 
+// Get data about movies by genre by name
+app.get('/genres/:id/name', (req, res) => {
+    res.send('Successful GET request returning movie by name of genre');
+});
 
-// Get data about movies by genre
-app.get('/movies/:genre', (req, res) => {
-    res.send('Successful GET request returning movie by genre');
+// Get data about movies by genre by name
+app.get('/genres/:id/genreName', (req, res) => {
+    let movie = topTenMovies.find(m => m.genre === req.params.genreName)
+    res.json(movie)
+});
+
+
+// Get data about movies by genre by description
+app.get('/genres/:id/description', (req, res) => {
+    res.send('Successful GET request returning movie by description of genre');
 });
 
 //Get data of a director's information by name
-app.get('/directors/:name', (req, res) => {
+app.get('/directors/:id/name', (req, res) => {
     res.send('Successful GET request of directors information.');
+});
+
+//Get data of a director's information by biography
+app.get('/directors/:id/biography', (req, res) => {
+    res.send('Successful GET request of directors biography.');
 });
 
 // Allow new users to register
@@ -125,12 +321,12 @@ app.post('/register', (req, res) => {
 });
 
 // Allow user to update their user info
-app.put('/users/:id/:user_info', (req, res) => {
+app.put('/users/:id', (req, res) => {
     res.send('Successful PUT to the server a user information.');
 });
 
 // Allow user to add a movie to their favorite movies list
-app.post('/users/:id/favorites', (req, res) => {
+app.patch('/users/:id/favorites', (req, res) => {
     res.send('Successful POST to the server a favorite movie on the user list.');
 });
 
@@ -140,7 +336,7 @@ app.delete('/users/:id/favorites', (req, res) => {
 });
 
 // Allow existing user to deregister
-app.delete('/users/:id/unregister', (req, res) => {
+app.delete('/users/:id', (req, res) => {
     res.send('Successful DELETE a user!');
 });
 
@@ -157,21 +353,3 @@ app.use((err, req, res, next) => {
 app.listen(8080, () =>{
     console.log('Your app is listening on port 8080.');
 });
-
-
-//Created a topTenMovies list.
-let topMovies = [
-    {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: '2003',
-    },
-    {
-    title: 'Pulp Fiction',
-    year: '1994',
-    },
-    {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: '2001',
-    }
-];
-
