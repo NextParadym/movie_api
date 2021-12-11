901a2e75acddf03cc76628d1076811900b6ac1c5
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-12-11 10:27:58 +0100

    Updated

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ mongoose.connect("mongodb://localhost:27017/myComedyFlix", {
 });*/
 
 // Secured connection URI
-/*
+
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});*/
+});
 
 // Mongo Atlas Database (You comment this when you want to connect to local db)
-
+/*
 mongoose.connect(
   "mongodb+srv://appDBS:123JANETuche@myappdbs.sqaoz.mongodb.net/myappDBS?retryWrites=true&w=majority"
-);
+);*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
