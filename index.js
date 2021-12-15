b513db1588039b0026e5dd2177ca40e629e1b624
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-12-15 07:39:21 +0100

    Updates

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const Users = Models.User;
 });*/
 
 // Mongo Atlas Database (You comment this when you want to connect to local db)
+
 /*
 mongoose.connect(
   "mongodb+srv://appDBS:123JANETuche@myappdbs.sqaoz.mongodb.net/myappDBS?retryWrites=true&w=majority ",
@@ -29,6 +30,7 @@ mongoose.connect(
   }
 );
 */
+
 // Secured connection URI
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
